53338c50199c1c5a7da1c19d80b5638ab4888c21
Gleen Ryan <tjiagleenryan@gmail.com>
2026-08-11 20:28:48 +0800

    Initial Commit

---
+++ CH5_Biorocks/CH5_BiorocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH5_BiorocksApp.swift
+//  CH5_Biorocks
+//
+//  Created by Gleenryan on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CH5_BiorocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH5_Biorocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH5_Biorocks
+//
+//  Created by Gleenryan on 11/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
